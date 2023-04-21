136f781584d9767e771c47b8cc820aa115c31bb4
Vang662 <119367547+Vang662@users.noreply.github.com>
2023-04-20 21:22:13 -0500

    jss/keyword

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -132,3 +132,18 @@ function saveSearchedKeyword() {
     localStorage.setItem("keys", JSON.stringify(keys));
   }
 }
+
+function getSearchedKeyword() {
+  var keys = JSON.parse(localStorage.getItem("keys")) || [];
+  for (let i = 0; i < keys.length; i++) {
+    $("#searchedKeywords").append("<li>" + keys[i] + "</li>");
+  }
+}
+
+// Delete created elements to ensure there are no duplicate appends when elements need to be appended again
+function deleteAppends() {
+  for (let num = 0; num < 3; num++) {
+    $("#attraction-" + num).empty();
+  }
+  $("#searchedKeywords").empty();
+}
